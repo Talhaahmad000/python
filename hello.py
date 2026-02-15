f0ace3b9317e04cb59b3f66f4057411c8f8df7f0
Talhaahmad000 <talhaahm100@gmail.com>
2026-02-15 22:19:44 +0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,4 @@
+name="talha";
+age=22;
+print(f"my name is:{name} and age is :{age}")#pre u write f and curly bracket in variable
+print("my name is",name ,"ur age is ",age)#"conflict bwt age is numebr and ur age is string mismatch datatype so , separate dtype "
